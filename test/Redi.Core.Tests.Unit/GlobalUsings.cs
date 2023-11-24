b558e63bda6a97b251bbe2c325cb1a5986818df0
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-11-23 20:47:47 -0500

    Checkpoint commit
    Remove Collection; Move structs to values folder; Create test project;

---
+++ test/Redi.Core.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// -----------------------------------------------------------------------
+// <copyright file="GlobalUsings.cs" company="Hazel Eclipse">
+// Copyright (c) Hazel Eclipse. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+global using Xunit;
